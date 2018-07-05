a5e516a89863d5c885f4b180d276e888118ed906
leoliu <leoliu@leoliudeMacBook-Pro.local>
2018-07-05 23:07:36 +0800

    banner

---
+++ LLKit/LLKit/Demo/IGListKit/DMBannerCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DMBannerCollectionViewCell.h
+//  LLKit
+//
+//  Created by leoliu on 2018/7/5.
+//  Copyright © 2018年 leoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMBannerCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ LLKit/LLKit/Demo/IGListKit/DMBannerCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  DMBannerCollectionViewCell.m
+//  LLKit
+//
+//  Created by leoliu on 2018/7/5.
+//  Copyright © 2018年 leoliu. All rights reserved.
+//
+
+#import "DMBannerCollectionViewCell.h"
+
+@implementation DMBannerCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
